4e45e36c9274b14d249f206321c3b4a2ed6f86b1
Anton Domashnev <antondomashnev@gmail.com>
2017-07-24 10:51:02 +0200

    Initial commit;

---
+++ SourceryTest/Foo.swift
@@ -0,0 +1,16 @@
+//
+//  Foo.swift
+//  SourceryTest
+//
+//  Created by Anton Domashnev on 24.07.17.
+//  Copyright © 2017 Antondomashnev. All rights reserved.
+//
+
+import Foundation
+
+class Service {
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+}
